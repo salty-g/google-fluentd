74b22e242b563c0f642edf6decf727d14b63ee4d
Dhrupad Bhardwaj <dhrupad@google.com>
2017-08-28 19:04:10 -0400

    Address feedback

--- config/software/jemalloc.rb
+++ config/software/jemalloc.rb
@@ -1,14 +1,21 @@
 name "jemalloc"
 default_version "4.2.1"
 
-hashes = { "2.2.5" => 'a5c4332705ed0e3fff1ac73cfe975640', "3.6.0" => 'e76665b63a8fddf4c9f26d2fa67afdf2', "4.2.1" => '094b0a7b8c77c464d0dc8f0643fd3901' }
-
 # for td-agent
-hashes.each do |var, hash|
-  version("#{var}") do
-    source :md5 => hash
-    source :url => "http://pkgs.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{hash}/jemalloc-#{version}.tar.bz2"
-  end
+version("2.2.5") do
+  md5 = 'a5c4332705ed0e3fff1ac73cfe975640'
+  source :md5 => md5
+         :url => "http://pkgs.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{md5}/jemalloc-#{version}.tar.bz2"
+end
+version("3.6.0") do
+  md5 = 'e76665b63a8fddf4c9f26d2fa67afdf2'
+  source :md5 => md5
+         :url => "http://pkgs.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{md5}/jemalloc-#{version}.tar.bz2"
+end
+version("4.2.1") do
+  md5 = '094b0a7b8c77c464d0dc8f0643fd3901'
+  source :md5 => md5
+         :url => "http://pkgs.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{md5}/jemalloc-#{version}.tar.bz2"
 end
 
 # On Mac, this file blocks package building at health check so add to whitelist
