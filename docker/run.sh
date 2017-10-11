3f637bc99747284ddb8b3fbbce998367e6ee7676
Bryan Moyles <bmoyles@google.com>
2017-10-11 15:52:41 -0400

    Address feedback.

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,9 @@
-# For systems without journald
+# For systems without journald.
 mkdir -p /var/log/journal
 
-# Copy host libsystemd into image to avoid compatibility issues.
+# Copy host libsystemd into image to avoid compatibility issues. This operation
+# is only performed if the host's lib is mounted into the container through a 
+# volume such as -v /usr/lib64/:/host/lib/.
 if [ ! -z "$(ls /host/lib/libsystemd* 2>/dev/null)" ]; then
   rm /lib/x86_64-linux-gnu/libsystemd*
   cp -a /host/lib/libsystemd* /lib/x86_64-linux-gnu/
