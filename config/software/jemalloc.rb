0194ed79d4992997d1fddcd57fd28231fda6cf3b
Dhrupad Bhardwaj <dhrupad@google.com>
2018-02-09 09:45:11 -0500

    Update jemalloc to latest -> 5.0.1

--- config/software/jemalloc.rb
+++ config/software/jemalloc.rb
@@ -1,5 +1,5 @@
 name "jemalloc"
-default_version "4.2.1"
+default_version "5.0.1"
 
 # for td-agent
 version("2.2.5") do
@@ -16,6 +16,11 @@ version("4.2.1") do
   md5 = '094b0a7b8c77c464d0dc8f0643fd3901'
   source :md5 => md5,
          :url => "http://src.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{md5}/jemalloc-#{version}.tar.bz2"
+end
+version("5.0.1") do
+  sha512 = '8cb5957a5724eb2bbad120cf0028ea8b2b14b4a416c1751b7c967351a7fd51135058ea0d3c4dc1d127c86f3aa7e9fd5ef101857110aabfdb7789427791c432c3'
+  source :sha512 => sha512,
+         :url => "http://src.fedoraproject.org/repo/pkgs/jemalloc/jemalloc-#{version}.tar.bz2/#{sha512}/jemalloc-#{version}.tar.bz2"
 end
 
 # On Mac, this file blocks package building at health check so add to whitelist
