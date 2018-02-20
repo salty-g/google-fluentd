86a47e64e7d13813c5a7b28df03a7d88d3c2ada5
Bryan Moyles <bmoyles@google.com>
2018-02-20 15:36:23 -0500

    Use entrypoint instead of command to capture kill signals.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # For systems without journald.
 mkdir -p /var/log/journal
 
@@ -9,4 +11,23 @@ if [ -n "$METADATA_AGENT_URL" ]; then
     /etc/google-fluentd/google-fluentd.conf
 fi
 
-/usr/sbin/google-fluentd "$@"
+trap 'kill $pid' TERM INT
+
+# first arg is a flag.
+if [ "${1:0:1}" = '-' ]; then
+  /usr/sbin/google-fluentd "$@" &
+else
+  exec "$@" &
+fi
+
+pid=$!
+wait $pid
+exit_code="$?"
+wait $pid
+
+# Ignore 143, as that is the code when SIGINT kills the process.
+if [[ exit_code -eq "143" ]]; then
+  exit 0
+fi
+
+exit $exit_code
