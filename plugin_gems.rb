52794e36374f69b49ed7d478cf891515645c8ad5
Igor Peshansky <igorp@google.com>
2019-05-29 16:12:51 -0400

    Remove unused and deprecated gems.

--- plugin_gems.rb
+++ plugin_gems.rb
@@ -1,27 +1,13 @@
 dir 'plugin_gems'
 # The order of these dependencies matters due to how Ruby pulls dependencies.
 download "httpclient", "2.7.2"
-download "td-client", "1.0.5"
-download "td", "0.15.8"
-download "fluent-plugin-td", "0.10.28"
-download "thrift", "0.8.0"
-download "fluent-plugin-scribe", "0.10.14"
 download "uuidtools", "2.1.5"
-download "bson", "1.12.5"
-download "bson_ext", "1.12.5"
-download "mongo", "1.12.5"
-download "fluent-plugin-mongo", "0.7.13"
 download "aws-sdk", "2.3.14"
 download "aws-sdk-v1", "1.66.0"
 download "fluent-plugin-s3", "0.8.4"
 download "webhdfs", "0.8.0"
 download "fluent-plugin-webhdfs", "0.4.2"
-download "string-scrub", "0.0.5"
 download "fluent-plugin-rewrite-tag-filter", "1.5.5"
-# Pin mixlib-cli to 1.7.0 until Ruby is upgraded to 2.5.
-# This is an indirect dependency of fluent-plugin-td-monitoring.
-download "mixlib-cli", "1.7.0"
-download "fluent-plugin-td-monitoring", "0.2.2"
 download "fluent-plugin-google-cloud", "0.7.13"
 download "fluent-plugin-detect-exceptions", "0.0.10"
 download "prometheus-client", "0.9.0"
