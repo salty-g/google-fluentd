7c83da3cb3ff2ba890c36d130fc7b14960a47018
Javier Kohen <jkohen@google.com>
2019-11-04 14:33:44 -0500

    install core_gems'

--- windows-installer/generate_sdl_agent_exe.ps1
+++ windows-installer/generate_sdl_agent_exe.ps1
@@ -152,7 +152,9 @@ $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";"
 ###############################
 
 $gem_installer = $SRC_ROOT + '\bin\gem_installer'
+$core_gems_rb = $SRC_ROOT + '\core_gems.rb'
 $plugin_gems_rb = $SRC_ROOT + '\plugin_gems.rb'
+& $RUBY_EXE $gem_installer $core_gems_rb
 & $RUBY_EXE $gem_installer $plugin_gems_rb
 
 ##############################
