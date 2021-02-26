66a13b7aa28ac08617a0dcb97ec946500ec05ea6
sophieyfang <sophieyfang@google.com>
2021-02-26 15:14:29 -0500

    Update generate_sdl_agent_exe.ps1

--- windows-installer/generate_sdl_agent_exe.ps1
+++ windows-installer/generate_sdl_agent_exe.ps1
@@ -146,7 +146,7 @@ $gem_installer = $SRC_ROOT + '\bin\gem_installer'
 $core_gems_rb = $SRC_ROOT + '\core_gems.rb'
 $plugin_gems_rb = $SRC_ROOT + '\plugin_gems.rb'
 # Pin ffi to version '1.14.1'. Remove the line when https://github.com/ffi/ffi/issues/868 is resolved.
-& $GEM_CMD install ffi --version '1.14.1'
+& $GEM_CMD install ffi:1.14.1 --no-document
 & $GEM_CMD install fluentd:1.11.2 --no-document
 & $RUBY_EXE $gem_installer $core_gems_rb
 & $RUBY_EXE $gem_installer $plugin_gems_rb
